9807cd7ecf4089cd565ef281774511f5dcb703af
Arjun gupta <rachnag352@gmail.com>
2025-03-12 00:07:39 +0530

    completed chatbot ui

--- Frontend/src/Components/ChatBot/Chatbot.jsx
+++ Frontend/src/Components/ChatBot/Chatbot.jsx
@@ -3,9 +3,30 @@ import { motion, AnimatePresence } from "framer-motion";
 import ChatIcon from "@mui/icons-material/Chat";
 import CloseIcon from "@mui/icons-material/Close";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import { TailSpin } from "react-loader-spinner";
 
 function Chatbot() {
   const [open, setOpen] = useState(false);
+  const [UserMessage, setUserMessage] = useState("");
+  const handleInputChange = (e) => {
+    setUserMessage(e.target.value);
+  };
+  const [messageStatus, setmessageStatus] = useState(false);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setmessageStatus(true);
+    // await axios
+    //   .post("https://taste-buds-treat-backend.vercel.app/send", formData)
+    //   .then((response) => {
+    //     setFormData({ name: "", email: "", message: "" });
+    //     toast.success("Message Sent Successfully!");
+    //   })
+    //   .catch((error) => {
+    //     toast.error("Failed to send message.");
+    //   });
+    setmessageStatus(false);
+  };
 
   return (
     <>
@@ -31,29 +52,83 @@ function Chatbot() {
       <AnimatePresence>
         {open && (
           <motion.div
-            initial={{ opacity: 0, y: 50 }} // Start hidden and below
-            animate={{ opacity: 1, y: 0 }} // Slide up into view
-            exit={{ opacity: 0, y: 50 }} // Slide down when closing
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 50 }}
             transition={{ duration: 0.3, ease: "easeInOut" }}
-            className="fixed bottom-20 right-5 w-80 h-96 bg-white shadow-xl rounded-xl m-4 border z-50"
+            className="fixed bottom-20 right-5 w-80 h-[27rem] bg-white shadow-xl rounded-xl m-4 border z-50 flex flex-col justify-between"
           >
-            {/* Chatbot Header */}
-            <div className="flex justify-between rounded-t-xl items-center p-2">
-              <div className="flex items-center">
-                <img
-                  src="https://res.cloudinary.com/dmxlqw5ix/image/upload/v1731066887/qxhi70lws9tx5ssy8ff3.png"
-                  alt="Logo"
-                  className="h-10"
-                />
-                <h2 className="text-lg font-semibold">Chatbot</h2>
+            <div className="flex flex-col overflow-auto">
+              {/* Chatbot Header */}
+              <div className="flex justify-between rounded-t-xl items-center p-2">
+                <div className="flex items-center">
+                  <img
+                    src="https://res.cloudinary.com/dmxlqw5ix/image/upload/v1731066887/qxhi70lws9tx5ssy8ff3.png"
+                    alt="Logo"
+                    className="h-10"
+                  />
+                  <h2 className="text-lg font-semibold font-WorkSans">
+                    Chatbot
+                  </h2>
+                </div>
+                <button className="mr-4" onClick={() => setOpen(false)}>
+                  <KeyboardArrowDownIcon />
+                </button>
               </div>
-              <button className="mr-4" onClick={() => setOpen(false)}>
-                <KeyboardArrowDownIcon />
-              </button>
-            </div>
 
-            {/* Chatbot Content */}
-            <p className="text-sm">This is where the chatbot will go...</p>
+              {/* Chatbot Content */}
+              <div className="mt-2 font-WorkSans flex flex-col gap-4 max-h-full overflow-y-auto">
+                {/* ChatBot dailog box */}
+                <div className="flex">
+                  <img
+                    src="https://res.cloudinary.com/dmxlqw5ix/image/upload/v1731066887/qxhi70lws9tx5ssy8ff3.png"
+                    alt="Logo"
+                    className="h-6 self-end"
+                  />
+                  <p className=" text-sm bg-green-100 p-2 rounded-xl">
+                    Hi There!👋
+                    <br />
+                    How can i help you today?
+                  </p>
+                </div>
+                {/* User content */}
+                <p className=" w-2/3 self-end text-sm p-2 text-white bg-green-400 rounded-xl mr-5">
+                  Lorem ipsum is a dummy or placeholder text commonly used in
+                  graphic design, publishing, and web development
+                </p>
+              </div>
+            </div>
+            {/* Message input field */}
+            <form className="" onSubmit={handleSubmit}>
+              <div className="flex items-center mt-4">
+                <input
+                  placeholder="Message.."
+                  type="text"
+                  name="UserMessage"
+                  value={UserMessage}
+                  onChange={handleInputChange}
+                  className=" text-black font-semibold px-4 py-2 mx-3 mb-4 border rounded-xl focus:outline-none focus:ring-2 focus:ring-green-500"
+                />
+                <button
+                  type="submit"
+                  className=" bg-green-400 rounded-full text-white flex items-center mb-4 ml-4"
+                >
+                  {messageStatus ? (
+                    <TailSpin
+                      height="15"
+                      width="15"
+                      color="#ffffff"
+                      ariaLabel="tail-spin-loading"
+                      radius="2"
+                      wrapperStyle={{ display: "inline-block" }}
+                      visible={true}
+                    />
+                  ) : (
+                    <KeyboardArrowUpIcon />
+                  )}
+                </button>
+              </div>
+            </form>
           </motion.div>
         )}
       </AnimatePresence>
